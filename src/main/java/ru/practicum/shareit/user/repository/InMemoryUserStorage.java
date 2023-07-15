536d5c86cf147097ef0064056d0307b11e16dfa2
Polina <cherkaschenkopo@gmail.com>
2023-07-15 22:43:28 +0300

    fix: первые правки по спринту 13

--- src/main/java/ru/practicum/shareit/user/repository/InMemoryUserStorage.java
+++ src/main/java/ru/practicum/shareit/user/repository/InMemoryUserStorage.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.exeption.AlreadyExistsException;
 import ru.practicum.shareit.exeption.NotFoundException;
-import ru.practicum.shareit.exeption.ValidationException;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.ArrayList;
